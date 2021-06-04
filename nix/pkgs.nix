65448d2ffed959d0f217e76d52f009de3427cb9c
Luke Gorrie <luke@nuddy.co>
2021-06-04 11:38:01 +0200

    nix/pkgs.nix: Update nixpkgs to release-21.05

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,2 +1,2 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs/archive/86233bdfddfc1cf8c302f68738205daab4df78db.tar.gz)
+import (fetchTarball https://github.com/NixOS/nixpkgs/archive/3903d2d41d95bca23f18c1b3d03299e549ab7415.tar.gz)
 
